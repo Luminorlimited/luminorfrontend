1e60571e0be52e317e98fa86371152b85e6b7455
Mobinul Islam Mahi <mobinulislammahi@gmail.com>
2025-02-08 19:44:20 +0600

    refactor: comment out unused imports and delete functionality in OffersTable component

--- src/components/dashboard/OffersTable.tsx
+++ src/components/dashboard/OffersTable.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import { Trash2 } from "lucide-react"
-import Swal from "sweetalert2"
-import { Button } from "@/components/ui/button"
+// import { Trash2 } from "lucide-react"
+// import Swal from "sweetalert2"
+// import { Button } from "@/components/ui/button"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 
 import { useGetTotalOfferQuery } from "@/redux/Api/dashboard/userapi"
-import { useDeleteOfferMutation } from "@/redux/Api/offerApi"
+// import { useDeleteOfferMutation } from "@/redux/Api/offerApi"
 
 
 
@@ -19,30 +19,30 @@ export default function OffersTable() {
 
 
 
-    const [deleteOffers] = useDeleteOfferMutation()
+    // const [deleteOffers] = useDeleteOfferMutation()
 
 
-    const handleDelete = (id: number) => {
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!",
-        }).then(async (result) => {
-            if (result.isConfirmed) {
-                try {
-                    await deleteOffers({ id }).unwrap(); // Assuming the API expects an object
-                    Swal.fire("Deleted!", "The offer has been deleted.", "success");
-                } catch (error) {
-                    Swal.fire("Error!", "Failed to delete the offer.", "error");
-                    console.error("Delete error:", error);
-                }
-            }
-        });
-    };
+    // const handleDelete = (id: number) => {
+    //     Swal.fire({
+    //         title: "Are you sure?",
+    //         text: "You won't be able to revert this!",
+    //         icon: "warning",
+    //         showCancelButton: true,
+    //         confirmButtonColor: "#3085d6",
+    //         cancelButtonColor: "#d33",
+    //         confirmButtonText: "Yes, delete it!",
+    //     }).then(async (result) => {
+    //         if (result.isConfirmed) {
+    //             try {
+    //                 await deleteOffers({ id }).unwrap(); // Assuming the API expects an object
+    //                 Swal.fire("Deleted!", "The offer has been deleted.", "success");
+    //             } catch (error) {
+    //                 Swal.fire("Error!", "Failed to delete the offer.", "error");
+    //                 console.error("Delete error:", error);
+    //             }
+    //         }
+    //     });
+    // };
 
 
 
@@ -62,7 +62,7 @@ export default function OffersTable() {
                         <TableHead>Receiver Name</TableHead>
                         <TableHead>Offer Type</TableHead>
                         <TableHead>Price</TableHead>
-                        <TableHead className="text-right">Action</TableHead>
+                        {/* <TableHead className="text-right">Action</TableHead> */}
                     </TableRow>
                 </TableHeader>
                 <TableBody>
@@ -74,14 +74,12 @@ export default function OffersTable() {
                             <TableCell>{offer?.clientEmail?.name?.firstName} {offer?.clientEmail?.name?.lastName}</TableCell>
                             <TableCell>{offer?.flatFee ? "Flat Fee" : offer?.hourlyFee ? "Hourly Fee" : "Milestone"}</TableCell>
                             <TableCell>{offer.totalPrice}</TableCell>
-                            <TableCell className="text-right">
-                                {/* <Button variant="ghost" className="hover:bg-green-200 text-green-800 rounded-[8px]" size="sm" onClick={() => handleEdit(offer)}>
-                                    <Pencil className="h-4 w-4" />
-                                </Button> */}
+                            {/* <TableCell className="text-right">
+                               
                                 <Button variant="ghost" className="hover:bg-red-200 text-red-800 rounded-[8px]" size="sm" onClick={() => handleDelete(offer._id)}>
                                     <Trash2 className="h-4 w-4" />
                                 </Button>
-                            </TableCell>
+                            </TableCell> */}
                         </TableRow>
                     ))}
                 </TableBody>
